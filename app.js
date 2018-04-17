4ead9280231dfa38d11213c67ffb020f92cac47d
Vagish Gupta <vagupta@BLR.ALLEGISINDIA.COM>
2018-04-17 19:29:24 +0530

    chenges app to env port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
-const port = process.env.Port || 3000
+const port = process.env.PORT || 3000
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
